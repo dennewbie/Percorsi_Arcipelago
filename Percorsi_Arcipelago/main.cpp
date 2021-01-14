dc22d7cde48a13c070268a18f264e24cddaf5301
DC <caruso.denny@gmail.com>
2021-01-14 20:51:19 +0100

    Initial Commit

---
+++ Percorsi_Arcipelago/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Percorsi_Arcipelago
+//
+//  Created by Denny Caruso on 14/01/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
